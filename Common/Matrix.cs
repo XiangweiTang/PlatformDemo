bd5754b05b7e6a00dcd1326acfe515dcf972d5d5
tangxiangwei@gmail.com <tangxiangwei@gmail.com>
2021-05-25 22:01:44 +0800

    Add matrix and MED.

---
+++ Common/Matrix.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Matrix<TRow, TColumn, TValue>
+    {
+        private Dictionary<TRow, Dictionary<TColumn, TValue>> InternalDict = new Dictionary<TRow, Dictionary<TColumn, TValue>>();
+        private TValue DefaultValue;
+        public HashSet<TRow> RowSet => _RowSet;
+        public int RowCount => _RowSet.Count;
+        private HashSet<TRow> _RowSet = new HashSet<TRow>();
+        public HashSet<TColumn> ColumnSet => _ColumnSet;
+        public int ColumnCount => _ColumnSet.Count;
+        private HashSet<TColumn> _ColumnSet = new HashSet<TColumn>();
+        public Matrix(TValue defaultValue)
+        {
+            DefaultValue = defaultValue;
+        }
+        public Matrix() { }
+            
+        public TValue this[TRow r, TColumn c]
+        {
+            get
+            {
+                Sanity.Requires(RowSet.Contains(r) && ColumnSet.Contains(c), $"The row-column pair [{r}-{c}] doesn't exist.");
+                Sanity.Requires(InternalDict.ContainsKey(r), "Mismatch in key sets.");
+                var dict = InternalDict[r];
+                return dict.ContainsKey(c) ? dict[c] : DefaultValue;
+            }
+            set
+            {
+                if (!RowSet.Contains(r))
+                {
+                    RowSet.Add(r);
+                    InternalDict.Add(r, new Dictionary<TColumn, TValue>());
+                }
+                if (!ColumnSet.Contains(c))
+                {
+                    ColumnSet.Add(c);
+                }
+                InternalDict[r][c] = value;
+            }
+        }
+
+        public Matrix<TColumn,TRow,TValue> Transpose()
+        {
+            Matrix<TColumn, TRow, TValue> m = new Matrix<TColumn, TRow, TValue>(DefaultValue);
+            foreach(TColumn c in ColumnSet)
+            {
+                foreach(TRow r in RowSet)
+                {
+                    m[c, r] = this[r, c];
+                }
+            }
+            return m;
+        }
+
+        public IEnumerable<string> Output(bool withColumnHeader = false, bool withRowHeader = false)
+        {
+            if (withColumnHeader)
+            {
+                if (withRowHeader)
+                    yield return string.Join("\t", "".Concat(_ColumnSet.Select(x => x.ToString())));
+                else
+                    yield return string.Join("\t", _ColumnSet);
+            }
+            foreach(var row in _RowSet)
+            {
+                if (withRowHeader)
+                {
+                    yield return string.Join("\t", row.ToString().Concat(DeDemensionRow(row).Select(x => x.ToString())));
+                }
+                else
+                {
+                    yield return string.Join("\t", DeDemensionRow(row));
+                }
+            }
+        }
+
+        public IEnumerable<TValue> DeDemensionColumn(TColumn c)
+        {
+            foreach (var r in _RowSet)
+                yield return this[r, c];
+        }
+        public IEnumerable<TValue> DeDemensionRow(TRow r)
+        {
+            foreach (var c in _ColumnSet)
+                yield return this[r, c];
+        }
+    }
+}
---
+++ Common/MinimumEditDistance.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class MinimumEditDistance<T> where T : IComparable
+    {
+        T[] StandardArray = null;
+        int SCount = -1;
+        T[] CompareArray = null;
+        int CCount = -1;
+        int[,] InternalMatrix = new int[0, 0];
+        public int DEL { get; private set; } = 0;
+        public int INS { get; private set; } = 0;
+        public int SUB { get; private set; } = 0;        
+        public MinimumEditDistance(T[] standardArray, T[] compareArray)
+        {
+            StandardArray = standardArray;
+            CompareArray = compareArray;
+            Init();
+        }
+
+        private void Init()
+        {
+            SCount = StandardArray.Length;
+            CCount = CompareArray.Length;
+        }
+
+        public int RunWithBacktrack()
+        {
+            InternalMatrix = new int[CCount + 1, SCount + 1];
+            for (int i = 1; i <= SCount; i++)
+                InternalMatrix[0, i] = i;
+            for (int i = 1; i <= CCount; i++)
+                InternalMatrix[i, 0] = i;
+
+            for(int i = 1; i <= CCount; i++)
+            {
+                for(int j = 1; j <= SCount; j++)
+                {
+                    int diag = InternalMatrix[i - 1, j - 1];
+                    if (!CompareArray[i - 1].Equals(StandardArray[j - 1]))
+                        diag++;
+                    int left = InternalMatrix[i, j - 1] + 1;
+                    int top = InternalMatrix[i - 1, j] + 1;
+                    int min = Math.Min(diag, Math.Min(left, top));
+                    InternalMatrix[i, j] = min;
+                }
+            }
+            return InternalMatrix[CCount, SCount];
+        }
+
+        public void BackTrace()
+        {
+            int i = CCount;
+            int j = SCount;
+            while (i >= 0 && j >= 0)
+            {
+                if (i == 0 && j == 0)
+                {
+                    break;
+                }
+                if (i == 0)
+                {
+                    j--;
+                    DEL++;
+                    continue;
+                }
+                if (j == 0)
+                {
+                    i--;
+                    INS++;
+                    continue;
+                }
+                int diag = InternalMatrix[i - 1, j - 1];                
+                int left = InternalMatrix[i, j - 1];
+                int top = InternalMatrix[i - 1, j];
+                int current = InternalMatrix[i, j];
+                if(current==diag&& StandardArray[j-1].Equals(CompareArray[i-1]))
+                {
+                    i--;
+                    j--;
+                    continue;
+                }
+                if(current==diag+1&&!StandardArray[j - 1].Equals(CompareArray[i - 1]))
+                {
+                    i--;
+                    j--;
+                    SUB++;
+                    continue;
+                }
+                if (current == top + 1)
+                {
+                    i--;
+                    INS++;
+                    continue;
+                }
+                if (current == left + 1)
+                {
+                    j--;
+                    DEL++;
+                    continue;
+                }
+                throw new CommonException("Mismatch in internal matrix.");
+            }
+        }
+    }
+}
